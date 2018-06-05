b8cc0b9b86197ac47d6f01d56f259527e3f1e24a
LUIS REQUEJO ROMERO <rsxano@LUISs-MacBook-Pro.local>
2018-06-05 02:07:49 -0300

    modulo 5

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
+import * as ContactsAPI from './utils/ContactsAPI'
+
+class App extends Component {
+
+  state = {
+    screen: 'list',
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts => {
+      this.setState({contacts})
+    }))
+  }
+
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+
+    ContactsAPI.remove(contact)
+  }
+  
+  render() {
+    return (
+      <div className="app">
+        {this.state.screen === 'list' && (
+          <ListContacts onDeleteContact={this.removeContact} 
+          contacts={this.state.contacts}
+          onNavigate={() => {
+            this.setState({ screen: 'create' })
+          }}
+           />
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact/>
+        )}
+      </div>
+    )
+  }
+}
+
+export default App;
